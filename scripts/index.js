e8b9274a7913e62ca926bb91841120ea6d1853bf
Fatima Mannaa <fatimamannaa0@gmail.com>
2026-08-31 12:03:10 +0300

    assignment

---
+++ scripts/index.js
@@ -0,0 +1,8 @@
+const username = "Fatima Mannaa";
+let age = 20;
+
+console.log(`Hello my name is ${username}. I'm ${age} years old`);
+
+age = 18;
+
+console.log(`2 years ago, I was ${age} years old`);
